1798c7b17a7fe2a5ced6d5aca49f6f23bf4e369a
iwanhae <iwanhae@gmail.com>
2023-01-16 06:49:30 +0900

    feat: add contact info

---
+++ src/lib/sections/contact.svelte
@@ -0,0 +1,44 @@
+<script>
+	import profileImg from '$lib/images/me.jpg';
+</script>
+
+<section>
+	<h1>Contact</h1>
+	<div class="container">
+		<p><b>Email (Personal)</b>: iwanhae [at] gmail [dot] com</p>
+		<p><b>Email (Academic)</b>: iwanhae [at] uos [dot] ac [dot] kr</p>
+		<p><b>Email (Kakao)</b>: scotty.scott [at] kakaocorp [dot] com</p>
+		<p>
+			<b>KakaoTalk</b>: iwanhae
+		</p>
+		<p>
+			<b>LinkedIn</b>:
+			<a href="https://www.linkedin.com/in/iwanhae/">https://www.linkedin.com/in/iwanhae/</a>
+		</p>
+		<p>
+			<b>Twitter</b>:
+			<a href="https://twitter.com/i_wanhae">https://twitter.com/i_wanhae</a>
+		</p>
+	</div>
+</section>
+
+<style>
+	section {
+		margin: auto;
+	}
+	.container {
+		display: grid;
+		width: 720px;
+		grid-template-columns: repeat(2, 1fr);
+	}
+	@media only screen and (max-width: 768px) {
+		.container {
+			display: grid;
+			width: 720px;
+			grid-template-columns: repeat(1, 1fr);
+		}
+	}
+	p {
+		overflow: auto;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script>
 	import About from '$lib/sections/about.svelte';
+	import Contact from '$lib/sections/contact.svelte';
 </script>
 
 <About />
+<Contact />
 
 <style>
 </style>
